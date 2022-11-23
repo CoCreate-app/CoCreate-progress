0d28e26a1d5724d60e5b19427aa1c8ed285d2152
Frank Pagan <frank@cocreate.app>
2022-11-23 20:50:45 +0000

    fix: bumped @cocreate dependencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ccfilter from '@cocreate/filter'
 import CRUD from '@cocreate/crud-client';
 
 let crud
-if(CRUD && CRUD.default)
+if (CRUD && CRUD.default)
 	crud = CRUD.default
 else
 	crud = CRUD
