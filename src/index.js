032ddae42b372610ca7b00e2b05da8d4655f8119
frankpagan <frank@cocreate.app>
2023-08-13 12:07:08 -0600

    feat: element.setValue and element.getValue replaces crud and filter

--- src/index.js
+++ src/index.js
@@ -1,94 +1,60 @@
-import ccfilter from '@cocreate/filter'
-import crud from '@cocreate/crud-client';
-
 var CoCreateProgress = {
+    selector: "[plugin='progress']",
+    // mainObjects: [],
 
-    selector: ".progress-wrapper",
+    init: function (element) {
+        if (element && !Array.isArray(element))
+            element = [element]
+        else if (!element)
+            element = document.querySelectorAll(this.selector);
 
-    mainObjects: [],
-
-    init: function () {
-        this.initElement()
-        this.initEvent()
-    },
-
-    initElement: function (container) {
-        const main_container = container ? container : document;
-
-        let elements = main_container.querySelectorAll(this.selector);
-        let _this = this;
-        elements.forEach((el) => {
-            let filter = ccfilter.init(el, 'data-progress_id', 'progress')
-
-            if (!filter) return;
-
-            let obj = {
-                el: el,
-                filter: filter,
-                id: el.getAttribute('data-progress_id')
+        for (let i = 0; i < element.length; i++) {
+            element[i].setValue = (data) => this.setData(element[i], data)
+            if (element[i].getValue) {
+                let value = element[i].getValue()
+                if (value)
+                    element[i].setValue(value)
             }
-            _this.mainObjects.push(obj);
-            _this.fetchProgess(el)
+            // element[i].getValue = () => self.getData(data)
 
-        })
-    },
-
-    initEvent: function () {
-        let _this = this;
-        crud.listen('read.object', function (data) {
-
-            if (data.metadata == "progress-total") {
-                _this.renderProgress(data, true);
-            } else if (data.metadata == "progress-value") {
-                _this.renderProgress(data, false)
-            }
-        })
-    },
-
-    renderProgress(data, isTotal) {
-        //.
-        if (!data) return;
-        const eid = data.element;
-        if (!eid) {
-            return;
         }
 
-        const result_count = data.object.length
-        let _this = this;
-        let elements = [];
-        let selector = isTotal ? '.progressTotal' : '.progressValue';
-        selector = selector + `[data-progress_id="${eid}"]`;
+    },
 
-        elements = document.querySelectorAll(selector)
+    setData: function (element, data) {
+        if (data.metadata == "progress-total") {
+            this.renderProgress(element, data, true);
+        } else if (data.metadata == "progress-value") {
+            this.renderProgress(element, data, false)
+        }
+    },
 
-        elements.forEach((el) => {
-            el.textContent = result_count;
-        })
+    renderProgress(element, data, isTotal) {
+        if (!data) return;
 
-        //. set progressbar
+        let elements = element.querySelectorAll(`.progressbar`)
 
-        elements = document.querySelectorAll(`.progressbar[data-progress_id="${eid}"]`)
-
-        elements.forEach((el) => {
-
-            if (el.tagName === "PROGRESS") {
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].tagName === "PROGRESS") {
                 if (isTotal) {
-                    el.setAttribute('max', result_count);
+                    elements[i].setAttribute('max', result_count);
                 } else {
-                    el.setAttribute('value', result_count);
+                    elements[i].setAttribute('value', result_count);
                 }
             } else {
                 if (isTotal) {
-                    el.setAttribute('data-total', result_count);
+                    elements[i].setAttribute('data-total', result_count);
                 } else {
-                    el.setAttribute('data-value', result_count);
+                    elements[i].setAttribute('data-value', result_count);
                 }
-                _this.renderBar(el)
+                this.render(elements[i])
             }
-        })
+
+        }
+
     },
 
-    renderBar: function (el) {
+    render: function (el) {
         const total = Number(el.getAttribute('data-total'));
         const value = Number(el.getAttribute('data-value'));
 
@@ -100,37 +66,37 @@ var CoCreateProgress = {
         el.innerHTML = `<div style="width: ${percent}%"></div>`;
     },
 
-    fetchProgess: function (el) {
-        let select_obj = null
-        let _id = el.getAttribute('data-progress_id')
+    //     fetchProgess: function (el) {
+    //         let select_obj = null
+    //         let _id = el.getAttribute('data-progress_id')
 
-        this.mainObjects.forEach((item) => {
-            if (item.id == _id) {
-                select_obj = item;
-            }
-        })
-        if (!select_obj) return;
+    //         this.mainObjects.forEach((item) => {
+    //             if (item.id == _id) {
+    //                 select_obj = item;
+    //             }
+    //         })
+    //         if (!select_obj) return;
 
-        let filter = select_obj.filter;
-        console.log(filter)
-        let totalFilter = { method: 'read.object', ...filter };
-        let valueFilter = { method: 'read.object', ...filter }
+    //         let filter = select_obj.filter;
+    //         console.log(filter)
+    //         let totalFilter = { method: 'read.object', ...filter };
+    //         let valueFilter = { method: 'read.object', ...filter }
 
-        let progressName = el.getAttribute('data-progress_name')
-        let progressValue = el.getAttribute('data-progress_value')
+    //         let progressName = el.getAttribute('data-progress_name')
+    //         let progressValue = el.getAttribute('data-progress_value')
 
-        let valueOperator = el.getAttribute('data-progress_operator') || "contain"
+    //         let valueOperator = el.getAttribute('data-progress_operator') || "contain"
 
-        totalFilter['metadata'] = 'progress-total';
-        valueFilter['metadata'] = 'progress-value'
+    //         totalFilter['metadata'] = 'progress-total';
+    //         valueFilter['metadata'] = 'progress-value'
 
-        let val_filter = [].concat(valueFilter['operator']['filters']);
-        val_filter.push({ key: progressName, value: [progressValue], operator: valueOperator });
-        valueFilter['operator']['filters'] = val_filter;
+    //         let val_filter = [].concat(valueFilter['operator']['filters']);
+    //         val_filter.push({ key: progressName, value: [progressValue], operator: valueOperator });
+    //         valueFilter['operator']['filters'] = val_filter;
 
-        crud.send(totalFilter)
-        crud.send(valueFilter)
-    }
+    //         crud.send(totalFilter)
+    //         crud.send(valueFilter)
+    //     }
 }
 
 CoCreateProgress.init();
